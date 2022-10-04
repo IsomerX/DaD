5c8e6606649b65ff27dfce3c7a8e46b0b3ca54e4
dhruv <dhruvbakshi12343@gmail.com>
2022-10-04 18:52:56 +0530

    format

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -1,13 +1,13 @@
 // @ts-check
-import { z } from "zod";
+import { z } from 'zod'
 
 /**
  * Specify your server-side environment variables schema here.
  * This way you can ensure the app isn't built with invalid env vars.
  */
 export const serverSchema = z.object({
-  NODE_ENV: z.enum(["development", "test", "production"]),
-});
+    NODE_ENV: z.enum(['development', 'test', 'production']),
+})
 
 /**
  * Specify your client-side environment variables schema here.
@@ -15,8 +15,8 @@ export const serverSchema = z.object({
  * To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 export const clientSchema = z.object({
-  // NEXT_PUBLIC_CLIENTVAR: z.string(),
-});
+    // NEXT_PUBLIC_CLIENTVAR: z.string(),
+})
 
 /**
  * You can't destruct `process.env` as a regular object, so you have to do
@@ -25,5 +25,5 @@ export const clientSchema = z.object({
  * @type {{ [k in keyof z.infer<typeof clientSchema>]: z.infer<typeof clientSchema>[k] | undefined }}
  */
 export const clientEnv = {
-  // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
-};
+    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+}
